8f095127c63cc3e8d99a96d2e20ac78cc666645c
Dylan Fiedler <dtfiedler@ge-gw.customer.alter.net>
2018-08-08 11:42:20 -0500

    adding string-palindrome

---
+++ string-palindrome.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//build a function that checks if a string is a palindrome
+let strings = ["abba", "racecar" , "false", "hannah"]
+
+func isPalindrome(s: String) -> Bool{
+    //break it into array of characters
+    let charArray = Array(s)
+    //get total number of characters
+    let count = s.count - 1
+    //go halfway through, check it's alternative is the same
+    for i in 0...count / 2 {
+        if charArray[i] != charArray[count - i]{
+            return false
+        }
+    }
+    return true
+}
+
+for s in strings {
+    if isPalindrome(s: s){
+        print("\(s) is a palindrome")
+    } else {
+        print("\(s) is not a palindrome")
+    }
+}
+
